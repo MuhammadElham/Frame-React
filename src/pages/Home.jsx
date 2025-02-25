0b9b38483e7198b7aed68d9e1af00799e31bc516
Elham <elhamwaheed777@gmail.com>
2025-02-25 16:45:57 +0500

    Working on Best Seller Components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "../component/Hero";
 import LatestCollection from "../component/LatestCollection";
+import BestSeller from "../component/BestSeller";
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <LatestCollection />
+      <BestSeller />
     </div>
   );
 };
