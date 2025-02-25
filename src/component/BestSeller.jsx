c3f389beb09ba222e30266c913f5a6b11eec5740
Elham <elhamwaheed777@gmail.com>
2025-02-25 20:30:56 +0500

    Best Seller Work Done

--- src/component/BestSeller.jsx
+++ src/component/BestSeller.jsx
@@ -1,13 +1,14 @@
 import React, { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../context/shopContext";
 import Title from "./Title";
+import ProductItem from "./ProductItem";
 
 const BestSeller = () => {
   const { products } = useContext(ShopContext);
   const [bestSeller, setBestSeller] = useState([]);
 
   useEffect(() => {
-    const bestProduct = products.filter((item) => item.bestSeller);
+    const bestProduct = products.filter((item) => item.bestseller);
     setBestSeller(bestProduct.slice(0, 5));
   }, []);
 
@@ -20,6 +21,18 @@ const BestSeller = () => {
           pariatur hic reiciendis sapiente aliquam totam vero sit veritatis,
           optio illo.
         </p>
+      </div>
+      {/* Products */}
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+        {bestSeller.map((item, index) => (
+          <ProductItem
+            key={index}
+            id={item._id}
+            image={item.image}
+            name={item.name}
+            price={item.price}
+          />
+        ))}
       </div>
     </div>
   );
