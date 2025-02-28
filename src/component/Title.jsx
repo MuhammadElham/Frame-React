046d5da8a51ab46adee27bb08e8a04ac2f049849
Elham <elhamwaheed777@gmail.com>
2025-02-28 17:09:09 +0500

    Resolve Responsiveness Back

--- src/component/Title.jsx
+++ src/component/Title.jsx
@@ -5,7 +5,8 @@ const Title = ({ text1, text2, lineWidth = "w-8 sm:w-12" }) => {
     // Latest Collection
     <div className="inline-flex gap-2 items-center mb-3">
       <p className="text-gray-500">
-        {text1} <span className="text-gray-700 font-medium">{text2}</span>
+        {text1}
+        <span className="text-gray-700 font-medium">{text2}</span>
       </p>
       <p className={`${lineWidth} h-[1px] sm:h-[2px] bg-gray-700`}></p>
     </div>
