12497bb3d4ca2e235b2a0ceb0ec9bd3e6dc879da
Elham <elhamwaheed777@gmail.com>
2025-03-09 03:06:26 +0500

    Testing Product Size

--- src/component/ProductItem.jsx
+++ src/component/ProductItem.jsx
@@ -8,7 +8,7 @@ const ProductItem = ({ id, image, name, price }) => {
     <Link to={`/product/${id}`} className="text-gray-700 cursor-pointer">
       <div className="overflow-hidden">
         <img
-          className="hover:scale-110 transition ease-in-out w-full h-[225px] lg:h-[370px] object-cover"
+          className="hover:scale-110 transition ease-in-out w-full h-[235px] lg:h-[370px] object-cover"
           src={image[0]}
           alt=""
         />
