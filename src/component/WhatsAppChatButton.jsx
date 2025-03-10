3c574d41b802dea6198a393d8cf7fd7ccfac22c8
Elham <elhamwaheed777@gmail.com>
2025-03-10 13:10:25 +0500

    Testing WhatsApp

--- src/component/WhatsAppChatButton.jsx
+++ src/component/WhatsAppChatButton.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const WhatsAppChatButton = () => {
-  const phoneNumber = "923333003310"; 
+  const phoneNumber = "923333003310";
   const message = "Hello! I need help."; // Default message
 
   const handleClick = () => {
@@ -9,22 +9,7 @@ const WhatsAppChatButton = () => {
   };
 
   return (
-    <button
-      onClick={handleClick}
-      style={{
-        position: "fixed",
-        bottom: "25px",
-        right: "25px",
-        backgroundColor: "#25D366",
-        color: "white",
-        borderRadius: "50%",
-        width: "70px",
-        height: "70px",
-        fontSize: "25px",
-        border: "none",
-        cursor: "pointer",
-      }}
-    >
+    <button onClick={handleClick} className="fixed bg-[#25D366] text-white rounded-full bottom-5 right-5 w-14 h-14 text-2xl lg:bottom-[25px] lg:right-[25px] lg:w-[70px] lg:h-[70px] lg:text-[25px] border-none cursor-pointer">
       💬
     </button>
   );
