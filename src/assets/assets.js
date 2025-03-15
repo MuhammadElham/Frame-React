ce5d801aa5839ba206cef8e31d964ef9132db292
Elham <elhamwaheed777@gmail.com>
2025-03-15 10:30:07 +0500

    Upload URL in About Page

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -11,7 +11,6 @@ import star_dull_icon from './star_dull_icon.png'
 import star_icon from './star_icon.png'
 import support_img from './support_img.png'
 import menu_icon from './menu_icon.png'
-import about_img from './about_img.png'
 import contact_img from './contact_img.png'
 import razorpay_logo from './razorpay_logo.png'
 import stripe_logo from './stripe_logo.png'
@@ -31,7 +30,7 @@ export const assets = {
     bin_icon,
     support_img,
     menu_icon,
-    about_img,
+    about_img:"https://res.cloudinary.com/dmmz8ldz9/image/upload/f_auto,q_auto/v1741605639/ecommerce-assets/iktuyiuthjwugpqnbqzn.png",
     contact_img,
     razorpay_logo,
     stripe_logo,
