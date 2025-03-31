9eb7f39276e098cba45cd5768d37657474787db0
Elham <elhamwaheed777@gmail.com>
2025-04-01 04:30:41 +0500

    WOrking in Cart Page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from "react";
+import { ShopContext } from "../context/ShopContext";
 
 const Cart = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const { products, currency, cartItems } = useContext(ShopContext);
+  const [cartData, setCartData] = useState([]); // Array
 
-export default Cart
+  useEffect(() => {
+    const tempData = [];
+    for (const items in cartItems) {
+      for (const item in cartItems[items]) {
+        if (cartItems[items][item] > 0) {
+          tempData.push({
+            _id: items,
+            size: item,
+            quantity: cartItems[items][item],
+          });
+        }
+      }
+    }
+    setCartData(tempData);
+  }, [cartItems]);
+
+  return <div></div>;
+};
+
+export default Cart;
