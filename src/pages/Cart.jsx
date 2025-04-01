e6956647dde288098d44092081bbb126f86c93ab
Elham <elhamwaheed777@gmail.com>
2025-04-01 05:38:07 +0500

    WOrking in Cart Page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../context/ShopContext";
+import Title from "../component/Title";
 
 const Cart = () => {
   const { products, currency, cartItems } = useContext(ShopContext);
@@ -21,7 +22,35 @@ const Cart = () => {
     setCartData(tempData);
   }, [cartItems]);
 
-  return <div></div>;
+  return (
+    <div className="border-t pt-14">
+      {/* Heading */}
+      <div className="text-2xl mb-3">
+        <Title text1={"YOUR "} text2={"CART"} />
+      </div>
+      {/* Item Listing */}
+      <div>
+        {cartData.map((item, index) => {
+          const productData = products.find((product) => product._id === item._id);
+          return (
+            <div key={index} className="py-4 border-t border-b text-gray-700">
+              <div className="flex items-start gap-6">
+                {/*Product Image */}
+                <img className="w-16 sm:w-20" src={Array.isArray(productData.image) ? productData.image[0] : productData.image} alt="" />
+                {/* Product Details */}
+                <div>
+                  <p className="text-xs sm:text-lg font-medium">{productData.name}</p>
+                  <div className="flex items-center gap-5 mt-2">
+                    <p>{currency}{productData.price}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        }, [])}
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
