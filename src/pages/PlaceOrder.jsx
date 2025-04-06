2716f80062697edd2a30852c2be2f85fda75d1c2
Elham <elhamwaheed777@gmail.com>
2025-04-06 22:09:23 +0500

    Applying Tawkto Api

--- src/pages/PlaceOrder.jsx
+++ src/pages/PlaceOrder.jsx
@@ -1,32 +1,67 @@
 import React from "react";
 import Title from "../component/Title";
+import CartTotal from "../component/CartTotal";
+import { assets } from "../assets/assets";
 
-const PlaceOrder = () => (
-  <div className="flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h-[80vh] border-t">
-    {/* -------- Left Side -------- */}
-    <div className="flex flex-col gap-4 w-full sm:max-w-[480px]">
-      {/* Heading */}
-      <div className="text-xl sm:text-2xl my-3">
-        <Title text1={"DELIVERY "} text2={"INFORMATION"} />
+const PlaceOrder = () => {
+  // const [method,setMethod] = useState('cod')
+
+  return (
+    <div className="flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h-[80vh] border-t">
+      {/* -------- Left Side -------- */}
+      <div className="flex flex-col gap-4 w-full sm:max-w-[480px]">
+        {/* Heading */}
+        <div className="text-xl sm:text-2xl my-3">
+          <Title text1={"DELIVERY "} text2={"INFORMATION"} />
+        </div>
+        {/* Input Fields */}
+        <div className="flex gap-3">
+          <input className="border border-gray-300 rounded py-1.5 px-3.5 w-full" type="text" placeholder="First name" />
+          <input className="border border-gray-300 rounded py-1.5 px-3.5 w-full" type="text" placeholder="Last name" />
+        </div>
+        <input className="border border-gray-300 rounded py-1.5 px-3.5 w-full" type="email" placeholder="Email address" />
+        <input className="border border-gray-300 rounded py-1.5 px-3.5 w-full" type="text" placeholder="Street" />
+        <div className="flex gap-3">
+          <input className="border border-gray-300 rounded py-1.5 px-3.5 w-full" type="text" placeholder="City" />
+          <input className="border border-gray-300 rounded py-1.5 px-3.5 w-full" type="text" placeholder="State" />
+        </div>
+        <div className="flex gap-3">
+          <input className="border border-gray-300 rounded py-1.5 px-3.5 w-full" type="number" placeholder="Zipcode" />
+          <input className="border border-gray-300 rounded py-1.5 px-3.5 w-full" type="text" placeholder="Country" />
+        </div>
+        <input className="border border-gray-300 rounded py-1.5 px-3.5 w-full" type="number" placeholder="Phone" />
       </div>
-      {/* Input Fields */}
-      <div className="flex gap-3">
-        <input className="border border-gray-300 rounded py-1.5 px-3.5 w-full" type="text" placeholder="First name" />
-        <input className="border border-gray-300 rounded py-1.5 px-3.5 w-full" type="text" placeholder="Last name" />
+
+      {/* -------- Right Side -------- */}
+      <div className="mt-8">
+        {/* Total */}
+        <div className="mt-8 min-w-80">
+          <CartTotal />
+        </div>
+        {/* Heading */}
+        <div className="mt-12">
+          <Title text1={"PAYMENT"} text2={"METHOD"} />
+          {/* ------- Payment Method Selection ------- */}
+          <div className="flex flex-col gap-3 lg:flex-row">
+            <div className="flex items-center gap-3 border p-2 px-3 cursor-pointer">
+              <p className={`min-w-3.5 h-3.5 border rounded-full`}></p>
+              <img className="h-5 mx-4" src={assets.stripe_logo} alt="" />
+            </div>
+
+            <div className="flex items-center gap-3 border p-2 px-3 cursor-pointer">
+              <p className={`min-w-3.5 h-3.5 border rounded-full`}></p>
+              <img className="h-5 mx-4" src={assets.razorpay_logo} alt="" />
+            </div>
+
+            <div className="flex items-center gap-3 border p-2 px-3 cursor-pointer">
+              <p className={`min-w-3.5 h-3.5 border rounded-full`}></p>
+              <p className="text-gray-500 text-sm font-medium mx-4">CASH ON DELIVERY</p>
+            </div>
+          </div>
+        </div>
       </div>
-      <input className="border border-gray-300 rounded py-1.5 px-3.5 w-full" type="email" placeholder="Email address" />
-      <input className="border border-gray-300 rounded py-1.5 px-3.5 w-full" type="text" placeholder="Street" />
-      <div className="flex gap-3">
-        <input className="border border-gray-300 rounded py-1.5 px-3.5 w-full" type="text" placeholder="City" />
-        <input className="border border-gray-300 rounded py-1.5 px-3.5 w-full" type="text" placeholder="State" />
-      </div>
-      <div className="flex gap-3">
-        <input className="border border-gray-300 rounded py-1.5 px-3.5 w-full" type="number" placeholder="Zipcode" />
-        <input className="border border-gray-300 rounded py-1.5 px-3.5 w-full" type="text" placeholder="Country" />
-      </div>
-      <input className="border border-gray-300 rounded py-1.5 px-3.5 w-full" type="number" placeholder="Phone" />
     </div>
-  </div>
-);
+  );
+};
 
 export default PlaceOrder;
