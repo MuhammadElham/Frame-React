20d633ed896a13efd00dff9bd6802d7e129ef82a
Elham <elhamwaheed777@gmail.com>
2025-04-18 02:27:20 +0500

    Related Product to Scroll Up on Clicking

--- src/component/ProductItem.jsx
+++ src/component/ProductItem.jsx
@@ -6,8 +6,15 @@ const ProductItem = ({ id, image, name, price }) => {
   const { currency } = useContext(ShopContext);
   const imageUrl = Array.isArray(image) ? image[0] : image;
 
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
   return (
-    <Link to={`/product/${id}`} className="text-gray-700 cursor-pointer">
+    <Link to={`/product/${id}`} onClick={scrollToTop} className="text-gray-700 cursor-pointer">
       <div className="overflow-hidden">
         <img className="hover:scale-110 transition ease-in-out w-full h-[235px] md:h-[300px] lg:h-[370px] object-cover" src={imageUrl} alt={name} />
       </div>
