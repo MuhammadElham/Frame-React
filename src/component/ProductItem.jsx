a0f4b6ae32c1eafcf2ef478e643dd5d185d5ccc7
Elham <elhamwaheed777@gmail.com>
2025-04-18 04:16:13 +0500

    Add PKR in Collection

--- src/component/ProductItem.jsx
+++ src/component/ProductItem.jsx
@@ -3,7 +3,7 @@ import { ShopContext } from "../context/ShopContext";
 import { Link } from "react-router-dom";
 
 const ProductItem = ({ id, image, name, price }) => {
-  const { currency } = useContext(ShopContext);
+  const { currency,currencyCode } = useContext(ShopContext);
   const imageUrl = Array.isArray(image) ? image[0] : image;
 
   const scrollToTop = () => {
@@ -22,6 +22,7 @@ const ProductItem = ({ id, image, name, price }) => {
       <p className="text-sm font-medium">
         {currency}
         {price}
+        {currencyCode}
       </p>
     </Link>
   );
