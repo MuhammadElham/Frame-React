0089cabdc9d0f165637f86d1755ecb27c6f6085f
Elham <elhamwaheed777@gmail.com>
2025-04-18 22:13:34 +0500

    Working in Order Page

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,13 +1,25 @@
 import React, { useContext } from "react";
 import { ShopContext } from "../context/ShopContext";
 import Title from "../component/Title";
+import { div } from "framer-motion/client";
 
 const Orders = () => {
   const { products, currency } = useContext(ShopContext);
   return (
     <div className="border-t pt-16">
+      {/* Heading */}
       <div className="text-2xl">
         <Title text1={"MY"} text2={"ORDERS"} />
+      </div>
+      {/* Product Details */}
+      <div>
+        {
+          products.slice(0,4).map((item,index)=>(
+            <div key={index} className="py-4 border-t border-b text-gray-700 flex flex-col md:flex-row md:items-center md:justify-between gap-4">
+                <img src={item.image[0]} alt="" />     
+            </div>
+          ))
+        }
       </div>
     </div>
   );
