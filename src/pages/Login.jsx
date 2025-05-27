6243aabb220ac8a0b0e822bd58d48bced95013f0
Elham <elhamwaheed777@gmail.com>
2025-05-27 21:03:46 +0500

    Working in Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -22,8 +22,18 @@ const Login = () => {
           toast.error(response.data.message);
         }
       } else {
+        const response = await axios.post(backendUrl + "/api/user/login", { email, password });
+        if (response.data.success) {
+          setToken(response.data.token);
+          localStorage.setItem("token", response.data.token);
+        } else {
+          toast.error(response.data.message);
+        }
       }
-    } catch (error) {}
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
   };
   return (
     <form onSubmit={onSubmitHandler} className="flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800">
