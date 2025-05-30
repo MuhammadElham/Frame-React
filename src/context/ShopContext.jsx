6df6d0649b4ec8f19ddde42beba33707d56fd266
Elham <elhamwaheed777@gmail.com>
2025-05-30 16:41:38 +0500

    Working Done for CartData

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -69,12 +69,23 @@ const ShopContextProvider = (props) => {
     }
     return totalCount;
   };
+
   // for Delete Item
   const updateQuantity = async (itemId, size, quantity) => {
     let cartData = structuredClone(cartItems);
     cartData[itemId][size] = quantity;
     setCartItems(cartData);
+    //
+    if (token) {
+      try {
+        await axios.post(backendUrl + "/api/cart/update", { itemId, size, quantity }, { headers: { token } });
+      } catch (error) {
+        console.log(error);
+        toast.error(error.message);
+      }
+    }
   };
+
   // for Total Amount
   const getCartAmount = () => {
     // temp var
@@ -109,6 +120,19 @@ const ShopContextProvider = (props) => {
       toast.error(error.message);
     }
   };
+  //
+  const getUserCart = async (token) => {
+    try {
+      const response = await axios.post(backendUrl + "/api/cart/get", {}, { headers: { token } });
+      if (response.data.success) {
+        setCartItems(response.data.cartData);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
+
   useEffect(() => {
     getProductsData();
   }, []);
@@ -116,6 +140,7 @@ const ShopContextProvider = (props) => {
   useEffect(() => {
     if (!token && localStorage.getItem("token")) {
       setToken(localStorage.getItem("token"));
+      getUserCart(localStorage.getItem("token"));
     }
   }, []);
   const value = {
