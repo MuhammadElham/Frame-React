469131eb4f4ce3699c275f750620af82b024cda4
Elham <elhamwaheed777@gmail.com>
2025-06-02 23:55:32 +0500

    Working Done in Order

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -51,7 +51,7 @@ const Orders = () => {
               {/* ---  Details  ---- */}
               <div>
                 <p className="sm:text-base font-medium">{item.name}</p>
-                <div className="flex items-center gap-3 mt-2 text-base text-gray-700">
+                <div className="flex items-center gap-3 mt-1 text-base text-gray-700">
                   <p>{currency}{item.price}{currencyCode}</p>
                   <p>Quantity: {item.quantity}</p>
                   <p className="flex items-center gap-2">
@@ -59,18 +59,17 @@ const Orders = () => {
                   </p>
                 </div>
                 {/* --- Date --- */}
-                <p className="mt-2">
-                  Date: <span className="text-gray-400">{new Date(item.date).toDateString()}</span>
-                </p>
+                <p className="mt-1">Date: <span className="text-gray-400">{new Date(item.date).toDateString()}</span></p>
+                <p className="mt-1">Payment: <span className="text-gray-400">{item.paymentMethod}</span></p>
               </div>
             </div>
             {/*  */}
             <div className="md:w-1/2 flex justify-between">
               <div className="flex items-center gap-2">
                 <p className="min-w-2 h-2 rounded-full bg-green-500"></p>
-                <p className="text-sm md:text-base">Ready to ship</p>
+                <p className="text-sm md:text-base">{item.status}</p>
               </div>
-              <button className="border px-4 py-2 text-sm font-medium rounded-sm">Track Order</button>
+              <button onClick={loadOrderData} className="border px-4 py-2 text-sm font-medium rounded-sm">Track Order</button>
             </div>
           </div>
         ))}
