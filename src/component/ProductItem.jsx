0dd571a9794a6e0350765c136603361d01fb00b3
Elham <elhamwaheed777@gmail.com>
2025-06-30 01:32:23 +0500

    Change Collection Image

--- src/component/ProductItem.jsx
+++ src/component/ProductItem.jsx
@@ -15,9 +15,12 @@ const ProductItem = ({ id, image, name, price }) => {
 
   return (
     <Link to={`/product/${id}`} onClick={scrollToTop} className="text-gray-700 cursor-pointer">
-      <div className="overflow-hidden">
-        <img className="hover:scale-110 transition ease-in-out w-full h-[235px] md:h-[300px] lg:h-[370px] object-cover" src={imageUrl} loading="lazy" alt={name} />
+      <div className="overflow-hidden rounded-md aspect-[3/4]">
+        <img className="w-full h-full object-cover hover:scale-110 transition duration-300 ease-in-out" src={imageUrl} loading="lazy" alt={name} />
       </div>
+      {/* <div className="overflow-hidden">
+        <img className="hover:scale-110 transition ease-in-out w-full h-[235px] md:h-[300px] lg:h-[370px] object-cover" src={imageUrl} loading="lazy" alt={name} />
+      </div> */}
       <p className="pt-3 pb-1 text-sm">{name}</p>
       <p className="text-sm font-medium">
         {currency}
