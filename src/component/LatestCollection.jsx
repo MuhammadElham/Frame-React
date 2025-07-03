dd2fa82e01e656d37b50e1c9d9d16e93d05508a5
Elham <elhamwaheed777@gmail.com>
2025-07-03 22:01:46 +0500

    Working in Content Writing

--- src/component/LatestCollection.jsx
+++ src/component/LatestCollection.jsx
@@ -14,23 +14,13 @@ const LatestCollection = () => {
   return (
     <div className="my-10">
       <div className="text-center py-8 text-3xl">
-        <Title text1={"LATEST "} text2={"COLLECTIONS"} forLatestCollection={'text-2xl sm:text-3xl'}/>
-        <p className="w-full sm:w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto
-          assumenda eaque aut saepe modi perspiciatis qui voluptas dolore, quae
-          non.
-        </p>
+        <Title text1={"LATEST "} text2={"COLLECTIONS"} forLatestCollection={"text-2xl sm:text-3xl"} />
+        <p className="w-full sm:w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">Discover our newest wedding treasures crafted with love and precision. Explore our fresh designs where tradition meets modern elegance.</p>
       </div>
       {/* Rendering Products */}
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
         {latestProduct.map((item, index) => (
-          <ProductItem
-            key={index}
-            id={item._id}
-            image={item.image}
-            name={item.name}
-            price={item.price}
-          />
+          <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price} />
         ))}
       </div>
     </div>
