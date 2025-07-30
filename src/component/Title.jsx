c1b25208e72ca0852e26c7a8b33353505961f7f3
Elham <elhamwaheed777@gmail.com>
2025-07-30 17:20:46 +0500

    Add Certificate 5:20

--- src/component/Title.jsx
+++ src/component/Title.jsx
@@ -4,7 +4,6 @@ const Title = ({ text1, text2, lineWidth = "w-6 sm:w-12", forLatestCollection })
   return (
     // Latest Collection
     <div className="inline-flex gap-2 items-center mb-3">
-      {/* <p className={`text-gray-500 ${forLatestCollection}`}>{text1} */}
       <p className={`text-[#d1a847] sm:font-semibold ${forLatestCollection}`}>{text1}
         <span className="text-gray-700 font-medium pl-1">{text2}</span>
       </p>
