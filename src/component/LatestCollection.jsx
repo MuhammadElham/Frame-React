6bb76dc2531463eff73e26b25a357689a95f9b4d
Elham <elhamwaheed777@gmail.com>
2025-07-30 17:12:57 +0500

    Add Icon in Heading 5:12\

--- src/component/LatestCollection.jsx
+++ src/component/LatestCollection.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../context/ShopContext";
 import Title from "./Title";
 import ProductItem from "./ProductItem";
+import Certificate from "../assets/certificate.png"
 
 const LatestCollection = () => {
   const { products } = useContext(ShopContext);
@@ -13,8 +14,9 @@ const LatestCollection = () => {
 
   return (
     <div className="my-10">
-      <div className="text-center py-8 text-3xl">
-        <Title text1={"LATEST "} text2={"COLLECTIONS"} forLatestCollection={"text-2xl sm:text-3xl"} />
+      <div className="flex flex-col items-center text-center py-8 text-3xl">
+        <img className="mb-3" src={Certificate} alt="" />
+        <Title text1={"NIKKAH CERTIFICATE "} text2={"COLLECTIONS"} forLatestCollection={"text-2xl sm:text-3xl"} />
         <p className="w-full sm:w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">Discover our newest wedding treasures crafted with love and precision. Explore our fresh designs where tradition meets modern elegance.</p>
       </div>
       {/* Rendering Products */}
