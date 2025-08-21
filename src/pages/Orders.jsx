2a5832002e04c9c5a5aa439027a512f7f8ba14e0
Elham <elhamwaheed777@gmail.com>
2025-08-22 04:15:18 +0500

    Data Save Successfully in DataBase Night 4:15

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -6,7 +6,7 @@ import { useEffect } from "react";
 import axios from "axios";
 
 const Orders = () => {
-  const { backendUrl, token, currency, currencyCode } = useContext(ShopContext);
+  const { products, backendUrl, token, currency, currencyCode } = useContext(ShopContext);
   const [orderData, setOrderData] = useState([]);
 
   const loadOrderData = async () => {
@@ -43,48 +43,52 @@ const Orders = () => {
       </div>
       {/* Product Details */}
       <div>
-        {orderData.map((item, index) => (
-          <div key={index} className="py-4 border-t border-b text-gray-700 flex flex-col md:flex-row md:items-center md:justify-between gap-4">
-            <div className="flex items-start gap-6 text-sm">
-              {/* ---  Images  --- */}
-              <img className="w-16 sm:w-20" src={Array.isArray(item.image) ? item.image[0] : item.image || "/placeholder.png"} alt="" />
-              {/* ---  Details  ---- */}
-              <div>
-                <p className="sm:text-base font-medium">{item.name}</p>
-                <div className="flex items-center gap-3 mt-1 text-base text-gray-700">
-                  <p>
-                    {currency}
-                    {item.price}
-                    {currencyCode}
-                  </p>
-                  <p>Quantity: {item.quantity}</p>
-                  {item.sizes?.length > 0 && (
-                    <p className="flex items-center gap-2">
-                      Size: <span className="px-3 py-1 text-sm border border-black bg-black text-white tracking-wider">{item.size}</span>
+        {orderData.map((item, index) => {
+          const productData = products.find((p) => p._id === item.productId);
+          return (
+            <div key={index} className="py-4 border-t border-b text-gray-700 flex flex-col md:flex-row md:items-center md:justify-between gap-4">
+              <div className="flex items-start gap-6 text-sm">
+                {/* ---  Images  --- */}
+                {/* <img className="w-16 sm:w-20" src={Array.isArray(item.image) ? item.image[0] : item.image || "/placeholder.png"} alt="" /> */}
+                <img className="w-16 sm:w-20" src={productData?.image?.[0]} alt={productData?.name} />
+                {/* ---  Details  ---- */}
+                <div>
+                  <p className="sm:text-base font-medium">{item.name}</p>
+                  <div className="flex items-center gap-3 mt-1 text-base text-gray-700">
+                    <p>
+                      {currency}
+                      {item.price}
+                      {currencyCode}
                     </p>
-                  )}
+                    <p>Quantity: {item.quantity}</p>
+                    {item.sizes?.length > 0 && (
+                      <p className="flex items-center gap-2">
+                        Size: <span className="px-3 py-1 text-sm border border-black bg-black text-white tracking-wider">{item.size}</span>
+                      </p>
+                    )}
+                  </div>
+                  {/* --- Date --- */}
+                  <p className="mt-1">
+                    Date: <span className="text-gray-400">{new Date(item.date).toDateString()}</span>
+                  </p>
+                  <p className="mt-1">
+                    Payment: <span className="text-gray-400">{item.paymentMethod}</span>
+                  </p>
                 </div>
-                {/* --- Date --- */}
-                <p className="mt-1">
-                  Date: <span className="text-gray-400">{new Date(item.date).toDateString()}</span>
-                </p>
-                <p className="mt-1">
-                  Payment: <span className="text-gray-400">{item.paymentMethod}</span>
-                </p>
+              </div>
+              {/*  */}
+              <div className="md:w-1/2 flex justify-between">
+                <div className="flex items-center gap-2">
+                  <p className="min-w-2 h-2 rounded-full bg-green-500"></p>
+                  <p className="text-sm md:text-base">{item.status}</p>
+                </div>
+                <button onClick={loadOrderData} className="border px-4 py-2 text-sm font-medium rounded-sm">
+                  Track Order
+                </button>
               </div>
             </div>
-            {/*  */}
-            <div className="md:w-1/2 flex justify-between">
-              <div className="flex items-center gap-2">
-                <p className="min-w-2 h-2 rounded-full bg-green-500"></p>
-                <p className="text-sm md:text-base">{item.status}</p>
-              </div>
-              <button onClick={loadOrderData} className="border px-4 py-2 text-sm font-medium rounded-sm">
-                Track Order
-              </button>
-            </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
     </div>
   );
